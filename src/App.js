7135f30a5b275bad0c96025a2edb3c59eb2988d6
pkbceira03 <cabeceira2003@gmail.com>
2023-05-01 11:16:05 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Olá React! <br />
+          oiii React! <br />
         </p>
         <a
           className="App-link"
