ed5cbe2a2c99379e4f66375cdfdf45f84b437e3b
pkbceira03 <cabeceira2003@gmail.com>
2023-05-03 08:52:20 -0300

    arrumando o src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          oiii React! <br />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>oi</h2>
     </div>
   );
 }
