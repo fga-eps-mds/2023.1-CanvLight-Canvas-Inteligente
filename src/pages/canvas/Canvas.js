9981935797f666b152aa318bcafd04d287146fa8
pkbceira03 <cabeceira2003@gmail.com>
2023-05-16 20:12:32 -0300

    form intuição. > > Co-authored-by: <brunotarquinio> Co-authored-by: <raquel-andrade>

--- src/pages/canvas/Canvas.js
+++ src/pages/canvas/Canvas.js
@@ -4,42 +4,153 @@ import logo from '../../images/logo_canvlight.png'
 import arrowIcon from '../../images/arrow.png';
 
 function Canvas() {
+  
+  function handleArrowClick() {
+    alert('Seta clicada!');
+  }
+
+  function abrirFormIntuicao(e){
+    e.preventDefault();
+
+    let modal = document.querySelector('.form');
+
+    modal.style.display = "block";
+  }
+
   return (
     <div className='body'>
         <div className='navbar'>
-                <div className='logo'>
-                    <img src={logo} alt='Logo' className='logo_navbar'/>
-                    <p className='name_navbar'>CanvLight</p>
-                </div>
+          <div className='logo'>
+            <img src={logo} alt='Logo' className='logo_navbar'/>
+            <p className='name_navbar'>CanvLight</p>
+          </div>
 
-                <div className='clear'></div>
-            </div>
+          <div className='clear'></div>
+        </div>
         <div className="container">
                 
-            <div className="rectangle">
-                <div className="dot"></div>
-                <div className="dot2"></div>
-                <div className="dot3"></div>
-                <button className='button1'>Intuição</button>
-                <button className='button2'>Observação</button>
-                <button className='button3'>Interação</button>
+          <div className="rectangle">
 
-                <div className="arrow" onClick={() => handleArrowClick()}></div> 
+            <div className="dot"></div>
+            <div className="dot2"></div>
+            <div className="dot3"></div>
+            <button className='button1' onClick={(e) => abrirFormIntuicao(e)}>Intuição</button>
+            <button className='button2'>Observação</button>
+            <button className='button3'>Interação</button>
 
-            </div>   
+            <div className="arrow" onClick={() => handleArrowClick()}></div> 
+
+          </div>   
         </div>
-            <img
-                src={arrowIcon}
-                alt="Seta"
-                className="arrow-button"
-                onClick={handleArrowClick}
-            />
+
+        <div className='form'>
+          <div className='parte1'>
+            <form>
+              <p>Projeto:</p>
+              <input type="text" />  
+
+              <p>Equipe:</p>
+              <input type="text" />  
+            </form>
+          </div>
+
+          <div className='tabela1'>
+            <table>
+              <tr>
+                <th>CONCEITO DNA</th>
+                <th>DNA DE NEGÓCIO</th>
+                <th>IMAGEM PAINEL SEMÂNTICO</th>
+              </tr>
+
+              <tr>
+                <td>TÉCNICO</td>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td>RESILIENTE</td>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td>EMOCIONAL</td>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td>MERCADOLÓGICO</td>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td>INTEGRADOR</td>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+            </table>
+          </div>
+
+          <div className='parte2'>
+            <p>ikigai</p>
+            <input type="text"/>
+          </div>
+
+          <div className='tabela2'>
+            <h2>4P's do projeto</h2>
+
+            <table>
+              <tr>
+                <td>P1:<input type="text"/></td>
+                <td>P2:<input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td>P3:<input type="text"/></td>
+                <td>P4:<input type="text"/></td>
+              </tr>
+            </table>
+          </div>
+
+          <div className='tabela3'>
+            <table>
+              <tr>
+                <th>POTENCIALIDADES</th>
+                <th>RESULTADOS ESPERADOS</th>
+              </tr>
+
+              <tr>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+              <tr>
+                <td><input type="text"/></td>
+                <td><input type="text"/></td>
+              </tr>
+
+            </table>
+          </div>
+        </div>
+
+        <img
+          src={arrowIcon}
+          alt="Seta"
+          className="arrow-button"
+          onClick={handleArrowClick}
+        />
     </div>
   );
 }
 
-function handleArrowClick() {
-  alert('Seta clicada!');
-}
 
 export default Canvas;
