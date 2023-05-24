b7d5db293228f3fed767f927bb9134149440a75f
raquel-andrade <raqueelfa@gmail.com>
2023-05-24 00:12:49 -0300

    alteracoes no css do form de interacao

--- src/pages/canvas/Interacao.js
+++ src/pages/canvas/Interacao.js
@@ -32,6 +32,10 @@ function Interacao (){
         let medel = document.querySelector('.tela_2_form_interacao');
         medel.style.display = "none";
       }
+      function newTask(){
+        alert('teste')
+
+      }
 
 
     return (
@@ -129,15 +133,21 @@ function Interacao (){
         <div className='esquerda'>
             <div className='parte3_interacao'>
                 <div>
-                    <spam>Checklist: <div onClick={clicouNaDuvida} className='duvida'></div> </spam>
-                    <input type="text" /> 
+                    <p>Checklist <div onClick={clicouNaDuvida} className='duvida'></div> </p>
                 </div>
+                    <div className='Inserir_tarefa'>
+                    <input type = "text" id='input-new-task' placeholder='Inserir tarefa'></input> 
+                    <button className='Inserir_tarefa' onClick={(e) => newTask(e)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                    </svg>
+                    </button>
+                    </div>
             </div>
         <div className='direita'>
             <div className='parte4_interacao'>
                 <p>MVP: <div onClick={clicouNaDuvida} className='duvida'></div> </p> 
             </div>
-        </div>
             <div className='tabela_MVP'>
                 <table className='table_interacao_tela2'>
                 <tr>
@@ -170,6 +180,7 @@ function Interacao (){
                 </tr>
                 </table>
             </div>
+        </div>    
         </div>
         <div className='parte5_interacao'>
             <form>
