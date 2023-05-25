d8300480e3169bed6299a070b8d4d0bc28ddb32e
LimirioGuimaraes <limirioguimaraes@gmail.com>
2023-05-25 15:42:32 -0300

    arrumados bugs da segunda tela

--- src/pages/canvas/Interacao.js
+++ src/pages/canvas/Interacao.js
@@ -157,130 +157,131 @@ function newTask(){
                 </div>           
             </div>
         </div>
-
         <div className='tela_2_form_interacao'>
             <div className='center'>
                 <button onClick={(e) => fecharFormInteracaoTela2(e)}></button>
                 <div className='tela2'>
-                    <div className='esquerda'>
-                        <div className='parte3_interacao'>
-                            <div>
-                                <p>Checklist <div onClick={clicouNaDuvida} className='duvida'></div> </p>
-                            </div>
-                            <div className='Inserir_tarefa'>
-                                <input type = "text" id='input-new-task' placeholder='Inserir tarefa'></input> 
-                                <button className='Inserir_tarefa' onClick={(e) => newTask(e)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                                </svg>
-                                </button>
-                            </div>
-                        </div>
-                        <div className='direita'>
-                            <div className='parte4_interacao'>
-                                <p>MVP: <div onClick={clicouNaDuvida} className='duvida'></div> </p> 
-                            </div>
-                            <div className='tabela_MVP'>
-                                <table className='table_interacao_tela2'>
-                                <tr>
-                                    <th>PRODUTO</th>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                </tr>
-                                <tr >
-                                    <th>INDICADO PARA</th>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                </tr>
-                                <tr>
-                                    <th>ORIENTAÇÕES DE USO</th>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                </tr>
-                                <tr>
-                                    <th>RESULTADO</th>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                    <td><input type="text"/></td>
-                                </tr>
-                                </table>
-                            </div>
-                        </div>    
+                <div className='esquerda'>
+                    <div className='parte3_interacao'>
+                    <div>
+                        <p>Checklist: <div onClick={clicouNaDuvida} className='duvida'></div> </p>
                     </div>
-                    <div className='parte5_interacao'>
-                        <form>
-                            <div>
-                                <p>Formação do Preço: <div onClick={clicouNaDuvida} className='duvida'></div></p>
-                            </div>
-                        </form>
+                    <div className='Inserir_tarefa'>
+                        <input type="text" id='input-new-task' placeholder='Inserir tarefa'></input> 
+                        <button className='Inserir_tarefa' onClick={(e) => newTask(e)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                        </svg>
+                        </button>
                     </div>
-                        <div className='tabela_formacao'>
-                            <table className='table_interacao_tela2/'>
-                            <tr>
-                                <th>DESCRIÇÃO</th>
-                                <th>CUSTO</th>
-                                <th>TAXA ADMINISTRATIVA</th>
-                                <th>IMPOSTOS</th>
-                                <th>MARGEM DE LUCRO</th>
-                                <th>PREÇO</th>
-                            </tr>
-                            <tr>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                            </tr>
-                            <tr>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                                <td><input type="text"/></td>
-                            </tr>
-                            </table>
-                        </div>
-                    <div className='clear'></div>
-                    <img
-                        src={rightArrow}
-                        alt="Seta"
-                        className="right-arrow"
-                        onClick={(e) =>nextpage2(e)}
-                    />
-                    <img
+                    </div>
+                </div>
+                <div className='direita'>
+                    <div className='parte4_interacao'>
+                    <p>MVP: <div onClick={clicouNaDuvida} className='duvida'></div> </p> 
+                    </div>
+                    <div className='tabela_MVP'>
+                    <table className='table_interacao_tela2'>
+                        <tr>
+                        <th>PRODUTO</th>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                        <th>INDICADO PARA</th>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                        <th>ORIENTAÇÕES DE USO</th>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                        <th>RESULTADO</th>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        </tr>
+                    </table>
+                    </div>
+                </div>
+                <div className='parte5_interacao'>
+                    <form>
+                    <div>
+                        <p>Formação do Preço: <div onClick={clicouNaDuvida} className='duvida'></div></p>
+                    </div>
+                    </form>
+                </div>
+                <div className='tabela_formacao'>
+                    <table className='table_interacao_tela2'>
+                    <tr>
+                        <th>DESCRIÇÃO</th>
+                        <th>CUSTO</th>
+                        <th>TAXA ADMINISTRATIVA</th>
+                        <th>IMPOSTOS</th>
+                        <th>MARGEM DE LUCRO</th>
+                        <th>PREÇO</th>
+                    </tr>
+                    <tr>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    </table>
+                </div>
+                <div className='clear'></div>
+                <img
+                    src={rightArrow}
+                    alt="Seta"
+                    className="right-arrow"
+                    onClick={(e) =>nextpage2(e)}
+                />
+                <img
                     src={rightArrow}
                     alt="Seta"
                     className="left-arrow"
                     onClick={(e) =>backpage1(e)}
-                    />
+                />
                 </div>           
             </div>
-                </div>
+        </div>
+
+
         <div className='tela_3_form_interacao'>
             <div className='center'>
                 <div className='esquerda'>
