00d5ea4f9ea9f74217ad4af5810261d0e2b474da
raquel-andrade <raqueelfa@gmail.com>
2023-05-25 17:27:53 -0300

    arrumando bugs da tela 2

--- src/pages/canvas/Interacao.js
+++ src/pages/canvas/Interacao.js
@@ -27,10 +27,6 @@ function fecharFormInteracao(e){
     modal.style.display = "none";
 }
 
-function handleArrowClick() {
-    alert('Seta clicada!');
-}
-
 function nextpage1(e){
     e.preventDefault();
     let modal = document.querySelector('.tela_2_form_interacao');
@@ -169,7 +165,7 @@ function newTask(){
                     <div className='Inserir_tarefa'>
                         <input type="text" id='input-new-task' placeholder='Inserir tarefa'></input> 
                         <span className='button_tarefa' onClick={(e) => newTask(e)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="black" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="white" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                             </svg>
                         </span>
