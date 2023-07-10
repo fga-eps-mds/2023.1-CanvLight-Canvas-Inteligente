64116c98318cf1956b0c327edeca3ea0839c9ffd
LimirioGuimaraes <limirioguimaraes@gmail.com>
2023-07-10 19:12:48 -0300

    ajustes na responsividade da maturidade 1

--- src/components/Maturidade/Maturidade1.js
+++ src/components/Maturidade/Maturidade1.js
@@ -135,7 +135,7 @@ function Maturidade1({state}){
                 <button className="close_area1" onClick={(e) => fecharFormMaturidade1(e)}></button>
                 <div className="dot2_area1"></div>
                 <div className="dot3_area1"></div>
-                <h2 className="titulo">Área Administrativa{calculaAdm()}</h2>
+                <h2 className="titulo_area_adm">Área Administrativa  {calculaAdm()}</h2>
                 <div onClick={() => {window.open("tutorial#tut_canais_venda", "_blank");
                 }}className="duvidaAdm"></div>
 
