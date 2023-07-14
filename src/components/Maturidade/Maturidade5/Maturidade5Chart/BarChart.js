9d35017abbd54956edbc7b32adc8d0ccc3cfc20f
fsousac <felipescoelho517@gmail.com>
2023-07-14 16:49:42 -0300

    responsividade da mat5

--- src/components/Maturidade/Maturidade5/Maturidade5Chart/BarChart.js
+++ src/components/Maturidade/Maturidade5/Maturidade5Chart/BarChart.js
@@ -1,46 +1,49 @@
 import React from "react";
-import {Bar} from "react-chartjs-2"
+import { Bar } from "react-chartjs-2";
 // eslint-disable-next-line
-import {Chart as chartjs} from "chart.js/auto"
+import { Chart as chartjs } from "chart.js/auto";
 import "./BarChart.css";
 
-
-function BarChart({chartData}){
-
-    return <Bar data={chartData} options={{
+function BarChart({ chartData }) {
+  return (
+    <Bar
+      data={chartData}
+      options={{
         scales: {
-            x: {
-              ticks: {
-                color: 'var(--font_color)',
-                font: {
-                  size: 20,
-                },
+          x: {
+            ticks: {
+              color: "var(--font_color)",
+              font: {
+                size: 10,
               },
-              grid: {
-                display: false,
-              }
             },
-            y: {
-                ticks: {
-                  color: 'var(--font_color)',
-                  font: {
-                    size: 20,
-                  },
-                  stepSize: 10,
-                },
-                grid: {
-                    display: false,
-                },
-                min: 0,
-                max: 100,
+            grid: {
+              display: false,
             },
+          },
+          y: {
+            ticks: {
+              color: "var(--font_color)",
+              font: {
+                size: 10,
+              },
+              stepSize: 10,
+            },
+            grid: {
+              display: false,
+            },
+            min: 0,
+            max: 100,
+          },
         },
         plugins: {
-            legend: {
-              display: false
-            },
+          legend: {
+            display: false,
+          },
         },
-    }}/>;
+      }}
+    />
+  );
 }
 
 export default BarChart;
